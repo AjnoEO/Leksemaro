43e126fcce26512f796227b29c397ce1fc8fe5ab
Ajno <boyko.artiom@gmail.com>
2025-03-07 18:37:03 +0300

    Исправил тест

--- lexicon/tests.py
+++ lexicon/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class UserModelTests(TestCase):
     def test_user_creation(self):
-        user = User(username="test_user", password="test_password")
-        user.save()
-        self.assertIsNotNone(user)
-        self.assertEqual(user.get_username(), "test_user")
+        user = User.objects.create(username="test_user", password="test_password")
+        user_id = user.pk
+        self.assertEqual(User.objects.get(pk=user_id).get_username(), "test_user")
+        self.assertListEqual(list(User.objects.all()), [user])
