aac30d7695fcc11c06fad7730924ebd8512efb6c
Ajno <boyko.artiom@gmail.com>
2025-03-11 16:41:24 +0300

    Исправил переадресацию на форму для входа

--- lexicon/views.py
+++ lexicon/views.py
@@ -3,20 +3,20 @@ from django.views import generic
 
 from .models import Language, WordClass
 
-class LanguageListView(generic.ListView, LoginRequiredMixin):
+class LanguageListView(LoginRequiredMixin, generic.ListView):
     template_name = "lexicon/language_list.html"
     context_object_name = "languages"
 
     def get_queryset(self):
         return Language.objects.filter(user=self.request.user)
 
-class LanguageView(generic.DetailView, LoginRequiredMixin):
+class LanguageView(LoginRequiredMixin, generic.DetailView):
     template_name = "lexicon/language.html"
 
     def get_queryset(self):
         return Language.objects.filter(user=self.request.user)
 
-class WordClassView(generic.DetailView, LoginRequiredMixin):
+class WordClassView(LoginRequiredMixin, generic.DetailView):
     template_name = "lexicon/word_class.html"
     context_object_name = "word_class"
 
