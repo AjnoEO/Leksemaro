1746381a18013564f8c176ad8f394a0d4bb4645c
Ajno <boyko.artiom@gmail.com>
2025-03-21 13:15:57 +0300

    Вынес общий функционал форм в отдельный класс

--- lexicon/urls.py
+++ lexicon/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.LanguageListView.as_view(), name="index"),
     path("language/<int:pk>", views.LanguageView.as_view(), name="language"),
     path("wordclass/<int:pk>", views.WordClassView.as_view(), name="word_class"),
-    path("add_lexeme/<int:word_class_id>", views.LexemeCreateView.as_view(), name="add_lexeme")
+    path("add_lexeme/<int:word_class>", views.LexemeCreateView.as_view(), name="add_lexeme")
 ]
