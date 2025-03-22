cd059051b54bb80c462571b6a1bc2ff09afa7cb5
Ajno <boyko.artiom@gmail.com>
2025-03-22 18:30:35 +0300

    Добавил Meaning в админ-панель

--- lexicon/admin.py
+++ lexicon/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Language, WordClass, LexicalCategory, LexicalCategoryValue, Lexeme
+from .models import Language, WordClass, LexicalCategory, LexicalCategoryValue, Lexeme, Meaning
 
-admin.site.register([Language, WordClass, LexicalCategory, LexicalCategoryValue, Lexeme])
+admin.site.register([Language, WordClass, LexicalCategory, LexicalCategoryValue, Lexeme, Meaning])
